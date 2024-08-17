a894b961942ecd6d61e7633bd1c3c710ff188fcd
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-16 21:50:59 -0500

    Got sddm working with theme

--- modules/nixos/extras/graphics/display-manager.nix
+++ modules/nixos/extras/graphics/display-manager.nix
@@ -1,10 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
+let
+    themeConfig.General = {
+        passwordInputWidth = 0.1;
+        passwordFontSize = 18;
+
+        showSessionsByDefault = true;
+        sessionsFontSize = 24;
+        showUsersByDefault = true;
+        usersFontSize = 24;
+    };
+in
 {
     services.displayManager.sddm = {
         enable = true;
-        extraPackages = [
-            pkgs.where-is-my-sddm-theme
-        ];
-        theme = "where_is_my_sddm_theme";
+        theme = "where_is_my_sddm_theme_qt5";
     };
+
+    environment.systemPackages = with pkgs; [
+        (where-is-my-sddm-theme.override {
+            variants = [ "qt5" ];
+            inherit themeConfig;
+        })
+    ];
 }
