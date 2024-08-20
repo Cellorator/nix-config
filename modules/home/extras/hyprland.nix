14acf7fce91e03571c5a782684bc1f810c52be64
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-20 13:41:02 -0500

    Added some env vars for hyprland that make nvidia work better apparently

--- modules/home/extras/hyprland.nix
+++ modules/home/extras/hyprland.nix
@@ -52,6 +52,13 @@
             "$mod, mouse:272, movewindow"
             "$mod, mouse:273, resizewindow"
         ];
+
+        env = [
+            "LIBVA_DRIVER_NAME,nvidia"
+            "XDG_SESSION_TYPE,wayland"
+            "GBM_BACKEND,nvidia-drm"
+            "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        ];
     };
 
     wayland.windowManager.hyprland.settings.general = {
