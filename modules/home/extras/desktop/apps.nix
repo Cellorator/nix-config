7299aca062fc8c3e0fd7e10239cbb519e3a033b1
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-11-13 18:13:33 -0600

    Installed some programming stuff

--- modules/home/extras/desktop/apps.nix
+++ modules/home/extras/desktop/apps.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
+ pkgs, ... }:
 {
     home.packages = with pkgs; [
         thunderbird
         obsidian
         zotero
+        unityhub
+        godot_4
+        vscode
     ];
 
     programs = {
