1e687cf2dfa1cbc6c7d7f226ec2a2ae99d529931
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-11-13 18:17:23 -0600

    Enabled gamescope and gamemode

--- modules/nixos/extras/gaming/default.nix
+++ modules/nixos/extras/gaming/default.nix
@@ -3,4 +3,13 @@
         ./steam.nix
         ./aagl.nix
     ];
+
+    programs.gamescope = {
+        enable = true;
+        capSysNice = true;
+    };
+    programs.gamemode = {
+        enable = true;
+        enableRenice = true;
+    }
 }
